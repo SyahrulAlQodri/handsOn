100e402658c2eca891b260b3ac1b38a18b806c2f
SyahrulAlQodri <alqodrisyahrul@gmail.com>
2024-05-06 14:29:42 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"; 
+import "./style.css";
+const App = () => {
+  return (
+<div>
+  <div className="navbar-warpper">
+    <div className="navbar-conttainer">
+      <div>logo</div>
+      <div className="navbar-list">
+        <a href="">home</a>
+        <a href="">about</a>
+        <a href="">contact</a>
+      </div>
+    </div>
+  </div>
+</div>
+  )
+   
+}
+
+export default App
